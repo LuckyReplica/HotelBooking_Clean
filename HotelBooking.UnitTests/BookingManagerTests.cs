1f982a93d736c35cebbdce9f2a8e4303fbd40131
Nicolai <nicolaiwork@live.dk>
2020-09-29 18:13:15 +0200

    BookingManagerTest with moq

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -3,24 +3,45 @@ using System.Collections;
 using System.Collections.Generic;
 using HotelBooking.Core;
 using HotelBooking.UnitTests.Fakes;
+using Moq;
 using Xunit;
 
 namespace HotelBooking.UnitTests
 {
     public class BookingManagerTests
     {
-        private IBookingManager bookingManager;
-        private DateTime start;
-        private DateTime end;
+        private Mock<IBookingManager> bookingManager;
+        private Mock<IRepository<Customer>> customerRepository;
+        private Mock<IRepository<Room>> roomRepository;
+        private Mock<IRepository<Booking>> bookingRepository;
+        private DateTime start = DateTime.Today;
+        private DateTime end = DateTime.Today;
         private static int TenDays = 10;
         private static int TwentyDays = 20;
 
-        public BookingManagerTests(){
-            start = DateTime.Today.AddDays(TenDays);
-            end = DateTime.Today.AddDays(TwentyDays);
-            IRepository<Booking> bookingRepository = new FakeBookingRepository(start, end);
-            IRepository<Room> roomRepository = new FakeRoomRepository();
-            bookingManager = new BookingManager(bookingRepository, roomRepository);
+        public BookingManagerTests()
+        {
+            var rooms = new List<Room>
+            {
+                new Room { Id=1, Description="a" },
+                new Room { Id=2, Description="b" },
+                new Room { Id=3, Description="c" },
+            };
+            var customers = new List<Customer>
+            {
+                new Customer{ Id=1, Name="Karsten", Email="karsten@gmial.com" },
+                new Customer{ Id=2, Name="Holgert", Email="holgert@gmial.com" }
+            };
+            var bookings = new List<Booking>
+            {
+                new Booking{ Id=1, StartDate=start, EndDate=end.AddDays(12), Customer=customers[0], Room=rooms[0], IsActive=true },
+                new Booking{ Id=1, StartDate=start.AddDays(5), EndDate=end.AddDays(12), Customer=customers[1], Room=rooms[2], IsActive=true }
+            };
+
+            bookingRepository = new Mock<IRepository<Booking>>(start, end);
+            roomRepository = new Mock<IRepository<Room>>();
+            bookingManager = new Mock<IBookingManager>(bookingRepository, roomRepository);
+
         }
 
         public static IEnumerable<object[]> FindAvailableRoom_TestCases()
